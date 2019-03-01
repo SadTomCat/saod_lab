3a405589242b879f3cf106b53868ee286db93e73
SadTomCat <mrtrevor24@gmail.com>
2019-03-01 15:09:54 +0700

    selection and quick sorts commit

--- quick.c
+++ quick.c
@@ -3,27 +3,62 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-#define ARR_SIZE 11
-#define F 12.00
+#define ARR_SIZE 1000000
 
-//сортировка 
-int qsort(int *p_arr)
-{
-    printf("%d", *p_arr);
-    return *p_arr;
-}
+void quicks(uint32_t *arr, uint32_t first, uint32_t last);
 
 int main()
 {
     srand(time(NULL));
-    uint32_t arr[ARR_SIZE]; //неотсортированный массив
+    uint32_t arr[ARR_SIZE];
 
-    //заполнение массива 
-    for(int i = 0; i < ARR_SIZE; i++)
+    for(uint32_t i = 0; i < ARR_SIZE; i++)
     {
-        arr[i] = rand()%30;
+        arr[i] = rand() % 100;
     }
-    qsort(arr[ARR_SIZE / 2]);
-    printf("%d", qsort(arr[ARR_SIZE / 2]));
+    
+    signed long long int StartTime = time(NULL);
+    quicks(arr, 0, ARR_SIZE - 1);
+    uint32_t good = 0;	
+    for(uint32_t i = 0; i < ARR_SIZE; i++)
+    {
+		if(arr[i] <= arr[i + 1])
+		{
+			good++;
+		}
+	}
+	if(good == ARR_SIZE)
+		printf("\ngood\n");
+    signed long long int EndTime = time(NULL);
+    printf("%lli seconds\n", EndTime - StartTime);
+    
+}
 
+void  quicks(uint32_t *arr, uint32_t first, uint32_t last)
+{
+	uint32_t left = first;
+	uint32_t right = last;
+	uint32_t pivot = arr[(right + left) / 2];
+	uint32_t tmp;
+	
+	while(left <= right)
+	{
+		while(arr[left] < pivot)
+			left++;
+		while(pivot < arr[right])
+			right--;
+		if(left <= right)
+		{
+			tmp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = tmp;
+			left++;
+			right--;
+		}
+	}
+	
+	if(right > first)
+		quicks(arr, first, right);
+	if(last > left)
+		quicks(arr, left, last);
 }
--- selection.c
+++ selection.c
@@ -3,27 +3,49 @@
 #include <time.h>
 #include <inttypes.h>
 
-#define ARR_SIZE 1000000
+#define ARR_SIZE 10000
+
+void selection(uint32_t *arr);
 
 int main()
 {
     srand(time(NULL));
-    uint32_t arr[ARR_SIZE]; //неотсортированный массив
-    uint32_t swap;   //переменная нужная для обмена 
-    uint32_t min;  //указавает на элемент с которым произойдет обмен 
-
-    //заполнение массива 
-    for(int i = 0; i < ARR_SIZE; i++) 
+    uint32_t arr[ARR_SIZE]; 
+  
+    for(uint32_t i = 0; i < ARR_SIZE; i++) 
     {
         arr[i] = rand()%100000;
     }
 
     signed long long int StartTime = time(NULL);
-    //сортировка
-    for(int i = 0; i < ARR_SIZE - 1; i++) 
+	selection(arr);
+    signed long long int EndTime = time(NULL);
+    printf("%lli seconds\n", EndTime - StartTime);
+    
+    uint32_t good = 0;
+    for(uint32_t i = 0; i < ARR_SIZE; i++)
+    {
+		if(arr[i] <= arr[i + 1])
+		{
+			good++;
+		}
+	}
+	if(good == ARR_SIZE)
+		printf("\ngood\n");
+
+    return 0; 
+}
+
+void selection(uint32_t *arr)
+{
+	uint32_t min; 
+    uint32_t search;
+    uint32_t tmp;
+    
+	for(uint32_t i = 0; i < ARR_SIZE - 1; i++) 
     {
         min = i;
-        int search = i + 1;
+        search = i + 1;
 
         while(search < ARR_SIZE) 
         {
@@ -34,12 +56,8 @@ int main()
             search++;
         }
 
-        swap = arr[i];
+        tmp = arr[i];
         arr[i] = arr[min];
-        arr[min] = swap;
+        arr[min] = tmp;
     }
-    signed long long int EndTime = time(NULL);
-    printf("%lli seconds\n", EndTime - StartTime);
-
-    return 0; 
 }
